08da0c2b0f77bbff82b1972818b7eb3b7cab4d46
borpatika <borcsiczkypatrik@gmail.com>
2024-05-03 13:35:15 +0200

    "hibak" javitasa

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -38,6 +38,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
+    
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
